c3dfbc9cc5ad7132663dda4e65fc312c1db5c327
Christian Gruber <gruber@squareup.com>
2022-06-08 15:50:51 -0700

    Bump timeout

--- script/ci-sync.sh
+++ script/ci-sync.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 bazel_bin="${GITHUB_WORKSPACE}/bin/bazel"
+timeout=120
 
 set -x
-timeout 60 "${bazel_bin}" sync
+timeout ${timeout} "${bazel_bin}" sync
 while [ $? -ne 0 ]; do
-  timeout 60 "${bazel_bin}" sync
+  timeout ${timeout} "${bazel_bin}" sync
 done
